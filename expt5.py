477398aebe898c2e1a887190ffa7697692e5b78d
jadhav3116 <dipak23116jadhav@gmail.com>
2026-04-30 15:13:23 +0530

    Add files via upload

---
+++ expt5.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:12:00 2026
+
+@author: User
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 12:57:31 2026
+
+@author: harshvardhan Gaikwad
+"""
+student = {}
+student["name"] = "Rahul"
+student["age"] = 20
+student["course"] = "Python"
+print("Dictionary after adding elements:")
+print(student)
+student["age"] = 21
+print("\nDictionary after updating a value:")
+print(student)
+del student["course"]
+print("\nDictionary after deleting a key-value pair:")
+print(student)
