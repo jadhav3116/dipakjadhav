6ce76fadb36995dbf3421090155004ffa7a81d86
jadhav3116 <dipak23116jadhav@gmail.com>
2026-04-30 15:10:31 +0530

    Add files via upload

---
+++ expt4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:09:01 2026
+
+@author: dipak
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 13:12:56 2026
+
+@author: Harshvardhan Gaikwad
+"""
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+  for j in range(1, i + 1):
+     print(j, end=" ")
+  print()
