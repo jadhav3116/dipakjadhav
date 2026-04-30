9e3675b2b0e39f6c90e480a45bb3fb1ba9352263
jadhav3116 <dipak23116jadhav@gmail.com>
2026-04-30 15:50:52 +0530

    Add files via upload

---
+++ act4b.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:49:23 2026
+
+@author: dipak
+"""
+# Program to remove duplicate mobile numbers from a contact list
+
+# List of mobile numbers (with duplicates)
+numbers = [9876543210, 9123456780, 9876543210, 9988776655, 9123456780, 9988776655]
+
+# Removing duplicates using set()
+unique_numbers = list(set(numbers))
+
+# Displaying results
+print("Original contact list:", numbers)
+print("Contact list after removing duplicates:", unique_numbers)
