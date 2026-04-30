4fb6f17885b6cd888dcaa4ebd0e061fb93420ff7
jadhav3116 <dipak23116jadhav@gmail.com>
2026-04-30 15:44:45 +0530

    Add files via upload

---
+++ act3b.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:43:37 2026
+
+@author: dipak
+"""
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are allowed to watch the movie ")
+else:
+    print("Sorry, you are not eligible ")
