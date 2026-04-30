c4c4f59302e8a411cca5722d3a826403e546a4e1
jadhav3116 <dipak23116jadhav@gmail.com>
2026-04-30 15:45:55 +0530

    Add files via upload

---
+++ act3c.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:45:03 2026
+
+@author: User
+"""
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
