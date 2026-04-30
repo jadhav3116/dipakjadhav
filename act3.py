96cfeea984545e7f816f94e05228f78dd70e2f0b
jadhav3116 <dipak23116jadhav@gmail.com>
2026-04-30 15:43:04 +0530

    Add files via upload

---
+++ act3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:41:50 2026
+
+@author: dipak
+"""
+speed = float(input("Enter vehicle speed (km/h): "))
+
+if speed > 60:
+    print("Overspeeding! You have to pay a fine ")
+else:
+    print("Speed is within limit ")
